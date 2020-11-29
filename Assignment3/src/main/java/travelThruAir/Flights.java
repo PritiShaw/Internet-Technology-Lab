336d1f13eb3e78787f61f28d2adbffe373c489a0
PritiShaw <pritishaw0103@gmail.com>
2020-11-30 04:21:38 +0530

    Add Flight and Route Class

---
+++ Assignment3/src/main/java/travelThruAir/Flights.java
@@ -0,0 +1,67 @@
+package travelThruAir;
+
+public class Flight{
+    String flightNumber;
+    String departure;
+    String arrival;
+    int departureTime;
+    int arrivalTime;
+    int price;
+    int totalCost;
+
+    public Flight(String flightNumber, String departure, String arrival, int departureTime, int arrivalTime, int price){
+        this.flightNumber=flightNumber;
+        this.departure=departure;
+        this.arrival=arrival;
+        this.departureTime=departureTime;   
+        this.arrivalTime=arrivalTime;
+        this.price=price;
+        this.totalCost=-1;
+    }
+
+    public int getFlightNumber(){
+        return this.flightNumber;
+    }
+    
+    public int getDuration(){
+        int departureTimeMin = departureTime%100;
+        int departureTimeHr = departureTime/100;
+        int arrivalTimeMin = arrivalTime%100;
+        int arrivalTimeHr = arrivalTime/100;
+        int hrDiff = departureTimeHr - arrivalTimeHr;
+        int minDiff = departureTimeMin - arrivalTimeMin;
+      
+        if(minDiff<0){
+            minDiff += 60;
+            hrDiff -= 1;
+        }
+        else if(minDiff>59){
+            minDiff -= 60;
+            hrDiff += 1;
+        }
+        return hrDiff*100+minDiff;
+    }
+    public String getDuration(int i){
+        int duration = getDuration();
+        int durationMin = duration%100;
+        int durationHr = duration/100;
+        if(durationHr == 0)
+           return durationMin + " min";
+        if(durationMin == 0)
+           return durationHr + " hr";
+        return durationHr + "hr "  + durationMin + "min";
+    }
+
+    public int getDepartureTime(){
+        return this.departureTime;
+    }
+    public int getArrivalTime(){
+        return this.arrivalTime;
+    }
+    public int getPrice(){
+        return this.price;
+    }
+    public int getTotalCost(){
+        return this.totalCost;
+    }
+}
